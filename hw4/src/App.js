25b9e1d208610bdf876a850b748536923dc5f77e
anarchtsu <gontarenko.dev@gmail.com>
2022-04-19 23:13:51 +0300

    hm4

--- hw4/src/App.js
+++ hw4/src/App.js
@@ -1,6 +1,70 @@
+import axios from "axios";
+import {useState} from "react";
+import './App.css'
+
+class Note {
+    id;
+    content;
+
+    constructor(id, content) {
+        this.id = id;
+        this.content = content;
+    }
+}
+
 function App() {
+    const URL = 'http://localhost:7777/notes'
+    const [notes, setNotes] = useState([]);
+    const [content, setContent] = useState('');
+
+    const addNote = async (e) => {
+        e.preventDefault()
+        await axios.post(URL,{content:content})
+        await updateNotes()
+        setContent('')
+    }
+
+    const deleteNote = async (e, id) => {
+        e.preventDefault()
+        await axios.delete(URL + '/' + id)
+        await updateNotes()
+    }
+
+    const updateNotes = async () => {
+        let response = await axios.get(URL);
+        setNotes(response.data.map(x => new Note(x.id, x.content)))
+    }
+
     return (
-        <p>hw4</p>
+        <div>
+            <p>Notes</p>
+            <button onClick={e => {
+                e.preventDefault();
+                updateNotes();
+            }}>Update</button>
+            {(notes.map(note =>
+                <div className="note" key={note.id}>
+                    <p>{note.content}</p>
+                    <button onClick={e => deleteNote(e, note.id)}>X</button>
+                </div>
+            ))}
+            <br/>
+            <p>New Note</p>
+            <div className="note">
+                <form onSubmit={addNote}>
+                    <input
+                        type="text"
+                        id="content"
+                        onChange={(e) => {
+                            setContent(e.target.value)
+                        }}
+                        value={content}
+                        required
+                    />
+                    <button> Add</button>
+                </form>
+            </div>
+        </div>
     );
 }
 
